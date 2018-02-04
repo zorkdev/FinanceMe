2fe5bf6423d2d13ef36eed6aa441340623ec80ac
Attila Nemet <me@attilanemet.com>
2018-02-04 02:13:28 +0000

    Tweak

--- MyFinanceKit/Business/Validators.swift
+++ MyFinanceKit/Business/Validators.swift
@@ -10,8 +10,9 @@ public struct Validators {
 
     public static func validate(amount: String) -> Bool {
         let c = Formatters.currencySymbol
-        return amount.range(of: "^(\(c)?0\\.?|\(c)?0\\.\\d{0,2}|\(c)?[1-9]\\d{0,6}|\(c)?[1-9]\\d{0,6}\\.\\d{0,2})$",
-                            options: .regularExpression) != nil
+        let regex = "^(\(c)?0\\.?|\(c)?0\\.\\d{0,2}|\(c)?[1-9]\\d{0,6}|\(c)?[1-9]\\d{0,6}\\.\\d{0,2})$"
+
+        return amount.range(of: regex, options: .regularExpression) != nil
     }
 
 }
