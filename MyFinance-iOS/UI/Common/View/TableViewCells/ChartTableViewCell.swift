bb8bcf1c40fc9650aa51ec830a5e2b97a3e7de5b
Attila Nemet <me@attilanemet.com>
2018-05-03 23:24:28 +0100

    Add ChartCell

--- MyFinance-iOS/UI/Common/View/TableViewCells/ChartTableViewCell.swift
+++ MyFinance-iOS/UI/Common/View/TableViewCells/ChartTableViewCell.swift
@@ -4,4 +4,52 @@ class ChartTableViewCell: UITableViewCell {
 
     @IBOutlet weak var chart: LineChartView!
 
+    private weak var viewModel: ChartCellModelForViewType?
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        viewModel?.viewDelegate = nil
+        viewModel = nil
+    }
+
+}
+
+extension ChartTableViewCell: TableViewCellForViewModelType {
+
+    func update(viewModel: CellModelType) {
+        guard let viewModel = viewModel as? ChartCellModelForViewType else { return }
+
+        self.viewModel = viewModel
+        self.viewModel?.viewDelegate = self
+
+        update()
+    }
+
+}
+
+extension ChartTableViewCell: ChartCellModelViewDelegate {
+
+    func update() {
+        guard let viewModel = viewModel else { return }
+
+        chart.data = viewModel.data
+        chart.xAxis.setLabelCount(viewModel.data.entryCount, force: true)
+
+        chart.xAxis.valueFormatter = ChartCellModel.DateValueFormatter()
+        chart.rightAxis.valueFormatter = ChartCellModel.CurrencyValueFormatter()
+
+        chart.legend.enabled = false
+        chart.chartDescription?.enabled = false
+        chart.leftAxis.enabled = false
+        chart.rightAxis.drawGridLinesEnabled = false
+        chart.rightAxis.drawZeroLineEnabled = true
+        chart.xAxis.gridLineDashPhase = 5
+        chart.xAxis.gridLineDashLengths = [5]
+        chart.xAxis.labelPosition = .bottom
+        chart.rightAxis.setLabelCount(4, force: false)
+        chart.xAxis.avoidFirstLastClippingEnabled = true
+        chart.isUserInteractionEnabled = false
+    }
+
 }
---
+++ MyFinance-iOS/UI/Common/ViewModel/CellViewModels/ChartCellModel.swift
@@ -0,0 +1,86 @@
+import Charts
+
+protocol ChartCellModelViewDelegate: class {
+
+    func update()
+
+}
+
+protocol ChartCellModelForViewType: class {
+
+    var viewDelegate: ChartCellModelViewDelegate? { get set }
+
+    var data: LineChartData { get }
+
+}
+
+protocol ChartCellModelForViewModelType: CellModelType {
+
+    func update(endOfMonthSummaries: [EndOfMonthSummary])
+
+}
+
+class ChartCellModel {
+
+    class DateValueFormatter: NSObject, IAxisValueFormatter {
+        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            return Formatters.monthShort.string(from: Date(timeIntervalSince1970: value))
+        }
+    }
+
+    class CurrencyValueFormatter: NSObject, IAxisValueFormatter {
+        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            return Formatters.currencyNoFractions.string(from: NSNumber(value: value)) ?? ""
+        }
+    }
+
+    weak var viewDelegate: ChartCellModelViewDelegate?
+
+    var data: LineChartData
+
+    init(endOfMonthSummaries: [EndOfMonthSummary]) {
+        data = ChartCellModel.createChartData(from: endOfMonthSummaries)
+    }
+
+    private static func createChartData(from summaries: [EndOfMonthSummary]) -> LineChartData {
+        var dataEntries = [ChartDataEntry]()
+
+        for summary in summaries {
+            dataEntries.append(ChartDataEntry(x: summary.created.timeIntervalSince1970,
+                                              y: summary.balance))
+        }
+
+        let dataSet = LineChartDataSet(values: dataEntries, label: nil)
+        dataSet.drawValuesEnabled = false
+        dataSet.drawCirclesEnabled = false
+        dataSet.drawFilledEnabled = true
+        dataSet.lineWidth = 1
+        dataSet.colors = [ColorPalette.secondary]
+        dataSet.fillColor = ColorPalette.secondary.withAlphaComponent(0.5)
+
+        return LineChartData(dataSet: dataSet)
+    }
+
+}
+
+extension ChartCellModel: ChartCellModelForViewType {}
+
+extension ChartCellModel: ChartCellModelForViewModelType {
+
+    static var reuseIdentifier: String {
+        return ChartTableViewCell.reuseIdentifier
+    }
+
+    static var rowHeight: CGFloat {
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad: return 350
+        default: return 180
+        }
+    }
+
+    func update(endOfMonthSummaries: [EndOfMonthSummary]) {
+        data = ChartCellModel.createChartData(from: endOfMonthSummaries)
+        viewDelegate?.update()
+    }
+
+}
