7c07e163ed38f53dd2464c79cda27873d2f20bf2
Attila Nemet <me@attilanemet.com>
2018-10-20 00:34:02 +0100

    Fix parsing issue with Balance model

--- MyFinanceKit/Model/Balance.swift
+++ MyFinanceKit/Model/Balance.swift
@@ -4,7 +4,7 @@ public struct Balance: Storeable, Equatable {
     public let effectiveBalance: Double
     public let pendingTransactions: Double
     public let availableToSpend: Double
-    public let acceptedOverdraft: Double
+    public let acceptedOverdraft: Double?
     public let currency: String
     public let amount: Double
 
