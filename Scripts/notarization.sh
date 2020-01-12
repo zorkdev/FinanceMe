33cbc7ed80d89ee715d45968603c45e82c1af2dc
Attila Nemet <me@attilanemet.com>
2020-01-12 21:45:26 +0000

    Fix notarization

--- Scripts/notarization.sh
+++ Scripts/notarization.sh
@@ -4,6 +4,7 @@
 
 xcrun altool --notarize-app \
     --primary-bundle-id com.zorkdev.FinanceMe \
+    -itc_provider WX9VZ58J6W
     -f $XCS_OUTPUT_DIR/FinanceMe.app.zip \
     -u $APPLE_ID -p $APPLE_ID_PW \
 
