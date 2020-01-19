1042591abae2061754aa18c0c3045d2410cffc57
Attila Nemet <me@attilanemet.com>
2020-01-19 15:15:16 +0000

    Fix macOS frame

--- FinanceMeKit/FinanceMeKit/UI/Home/HomeView-macOS.swift
+++ FinanceMeKit/FinanceMeKit/UI/Home/HomeView-macOS.swift
@@ -33,6 +33,7 @@ struct HomeView: View {
         }
         .onAppear(perform: viewModel.onAppear)
         .errorBanner(errorViewModel)
+        .frame(minHeight: 600)
     }
 
     init(appState: AppState) {
