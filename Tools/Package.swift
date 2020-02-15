e18f79128d4c835f46f82ec1aa306a74cc2a03f7
Attila Nemet <me@attilanemet.com>
2020-02-15 14:02:33 +0000

    Update SwiftLint

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Tools",
     dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMajor(from: "0.38.2"))
+        .package(url: "https://github.com/realm/SwiftLint.git", from: "0.39.1")
     ],
     targets: [
         .target(name: "Tools", dependencies: ["swiftlint"])
